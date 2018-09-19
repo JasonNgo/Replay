e7a8571bae3508b9024e8712a9756403cd290afc
Jason Ngo <jason.n.ngo@gmail.com>
2018-09-19 17:40:50 -0400

    first commit

---
+++ Replay/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  Replay
+//
+//  Created by Jason Ngo on 2018-09-19.
+//  Copyright © 2018 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
